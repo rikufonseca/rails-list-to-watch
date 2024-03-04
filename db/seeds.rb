0be1b3eaee350b7dd68ef0466471971b025e2bb1
erika fonseca <fonsecarika@gmail.com>
2024-03-04 20:00:30 +0200

    seeds done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require "faker"
+require "json"
+require "open-uri"
+
+Movie.destroy_all
+
+puts "start seeds"
+
+#50.times do
+#  Movie.create(
+#    title: Faker::Movie.title,
+#    overview: Faker::Movie.quote,
+#    rating: rand(0..10)
+#  )
+#end
+
+url = "https://tmdb.lewagon.com/movie/top_rated"
+movies_serialized = URI.open(url).read
+result = JSON.parse(movies_serialized)
+movies = result["results"]
+
+movies.each do |movie|
+  Movie.create(
+  title: movie["title"],
+  overview: movie["overview"],
+  rating: movie["vote_average"],
+  poster_url: movie["poster_path"]
+  )
+end
+puts "seeds done"
